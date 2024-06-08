939fdda653b31440712de591f8f7598fbd878387
Luke Dawe <luke.dawe@gmail.com>
2024-06-08 16:04:07 +0100

    Somewhat configured TS so that it's compiling to a build directory. Working so that we have a webpage that is displaying the output of the TS code.

---
+++ build/graph_view.js
@@ -0,0 +1,7 @@
+"use strict";
+function greeter(person) {
+    return "Hello, " + person;
+}
+let user = "Jane User";
+document.body.textContent = greeter(user);
+document.body.textContent = greeter(user);
